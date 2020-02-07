c9ed6397682d7679f47c2b5685771280e9a4d631
Lee Smith <lee.smith@fundingcircle.com>
2020-02-07 13:49:44 +0000

    Use argparse

--- plot.py
+++ plot.py
@@ -1,19 +1,21 @@
-import sys
+import argparse
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as mani
 import requests
 import numpy as np
 
-ip = sys.argv[1]
-timestamp = sys.argv[2]
+parser = argparse.ArgumentParser()
+parser.add_argument('ip', help='IP address of the acceleration data web server')
+parser.add_argument('timestamp', help='Timestamp of the CSV data file')
+args = parser.parse_args()
 
 session = requests.Session()
 fig, axarr = plt.subplots(3, sharex=True)
 
-
 def animate(i):
     resp = session.get(
-        'http://{}:8080/xyz_{}.csv'.format(ip, timestamp),
+        'http://{}:8080/xyz_{}.csv'.format(args.ip, args.timestamp),
         stream=True
     ).raw
     components = np.loadtxt(resp, delimiter=',', unpack=True)
